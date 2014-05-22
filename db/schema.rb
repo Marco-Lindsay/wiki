d489ac39d6a0c5cb2a995f8a356a7af64211fe2c
marco-lindsay <lindsay_marco@hotmail.com>
2014-05-21 19:28:01 -0700

    add the join table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140522014450) do
+ActiveRecord::Schema.define(version: 20140522015909) do
 
   create_table "questions", force: true do |t|
     t.string   "title"
@@ -19,6 +19,11 @@ ActiveRecord::Schema.define(version: 20140522014450) do
     t.text     "answer"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "questions_users", force: true do |t|
+    t.integer "question_id"
+    t.integer "user_id"
   end
 
   create_table "users", force: true do |t|
